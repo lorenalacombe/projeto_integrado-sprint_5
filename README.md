690490b40c995d2e1127548f53b84785d97c2c7b
Lorena Lacombe <lorena.lacombe@gmail.com>
2025-01-16 23:19:54 -0300

    desenvolvimento do projeto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # projeto_integrado-sprint_5
 Dashboard do Projeto 5 do curso de Análise de Dados da TripleTen Brasil
+
+Link: 
--- projeto.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import plotly.express as px
-
-car_data = pd.read_csv('vehicles.csv')
-fig = px.histogram(car_data, x="odometer")
-fig.show()
